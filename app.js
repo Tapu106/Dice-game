89224a736476276e1ff943ff8449f7a0a77f3c15
Tapu106 <tapusust993@gmail.com>
2021-07-26 19:32:25 +0600

    init function added

--- app.js
+++ app.js
@@ -2,13 +2,27 @@ const diceBtn = document.querySelector(".dice");
 console.log(diceBtn);
 
 let score, roundScore, activePlayer, dice;
-score = [0, 0];
-roundScore = 0;
-activePlayer = 0;
+init = () => {
+  score = [0, 0];
+  roundScore = 0;
+  activePlayer = 0;
 
-//chnaging css style using DOM
-diceBtn.style.display = "none";
+  diceBtn.style.display = "none";
+  document.getElementById(`current-0`).textContent = "0";
+  document.getElementById(`current-1`).textContent = "0";
 
+  document.getElementById("score-0").textContent = "0";
+  document.getElementById("score-1").textContent = "0";
+
+  document.querySelector(".player-0-panel").classList.remove("winner");
+  document.querySelector(".player-1-panel").classList.remove("winner");
+  document.querySelector(".player-0-panel").classList.remove("active");
+  document.querySelector(".player-1-panel").classList.remove("active");
+  document.querySelector(".player-0-panel").classList.add("active");
+
+  document.getElementById("name-0").textContent = "Player 1";
+  document.getElementById("name-1").textContent = "Player 2";
+};
 // rolling dice event
 document.querySelector(".btn-roll").addEventListener("click", () => {
   // Random number
@@ -58,3 +72,8 @@ turnToNextPlayer = () => {
 
   diceBtn.style.display = "none";
 };
+
+// new game event
+document.querySelector(".btn-new").addEventListener("click", init);
+
+init();
