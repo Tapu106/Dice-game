aa584885d347fcf5757f0aad8d080e41b0370abe
Tapu106 <tapusust993@gmail.com>
2021-07-26 19:45:32 +0600

    state variable added

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 const diceBtn = document.querySelector(".dice");
 console.log(diceBtn);
 
-let score, roundScore, activePlayer, dice;
+let score, roundScore, activePlayer, dice, isPlaying;
 init = () => {
   score = [0, 0];
   roundScore = 0;
   activePlayer = 0;
+  isPlaying = true;
 
   diceBtn.style.display = "none";
   document.getElementById(`current-0`).textContent = "0";
@@ -25,40 +26,46 @@ init = () => {
 };
 // rolling dice event
 document.querySelector(".btn-roll").addEventListener("click", () => {
-  // Random number
-  dice = Math.floor(Math.random() * 6) + 1;
-  // display result
-  diceBtn.style.display = "block";
-  diceBtn.src = `dice-${dice}.png`;
+  if (isPlaying) {
+    // Random number
+    dice = Math.floor(Math.random() * 6) + 1;
+    // display result
+    diceBtn.style.display = "block";
+    diceBtn.src = `dice-${dice}.png`;
 
-  if (dice !== 1) {
-    // current player score increases
-    roundScore += dice;
-    document.getElementById(`current-${activePlayer}`).textContent = roundScore;
-  } else {
-    // next player
-    turnToNextPlayer();
+    if (dice !== 1) {
+      // current player score increases
+      roundScore += dice;
+      document.getElementById(`current-${activePlayer}`).textContent =
+        roundScore;
+    } else {
+      // next player
+      turnToNextPlayer();
+    }
   }
 });
 
 // hold event
 
 document.querySelector(".btn-hold").addEventListener("click", () => {
-  score[activePlayer] += roundScore;
-  document.getElementById(`score-${activePlayer}`).textContent =
-    score[activePlayer];
+  if (isPlaying) {
+    score[activePlayer] += roundScore;
+    document.getElementById(`score-${activePlayer}`).textContent =
+      score[activePlayer];
 
-  if (score[activePlayer] >= 20) {
-    document.getElementById("name-" + activePlayer).textContent = "Winner!";
-    diceBtn.style.display = "none";
-    document
-      .querySelector(`.player-${activePlayer}-panel`)
-      .classList.add("winner");
-    document
-      .querySelector(`.player-${activePlayer}-panel`)
-      .classList.remove("active");
-  } else {
-    turnToNextPlayer();
+    if (score[activePlayer] >= 20) {
+      document.getElementById("name-" + activePlayer).textContent = "Winner!";
+      diceBtn.style.display = "none";
+      document
+        .querySelector(`.player-${activePlayer}-panel`)
+        .classList.add("winner");
+      document
+        .querySelector(`.player-${activePlayer}-panel`)
+        .classList.remove("active");
+      isPlaying = false;
+    } else {
+      turnToNextPlayer();
+    }
   }
 });
 
